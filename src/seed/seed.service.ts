70739ab4cef2c9c477f30c8a9f289406ddd728cf
Ezequiel <zapata.ed1989@gmail.com>
2023-05-21 12:24:10 -0300

    Antes de insertar por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+  private readonly url: string = 'https://pokeapi.co/api/v2/pokemon?limit=650';
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(this.url);
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      console.log({ name, no });
+    });
+    return data.results;
+  }
+}
