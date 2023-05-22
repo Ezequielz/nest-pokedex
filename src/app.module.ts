a3b0cbc58c209d96aa7341de159ecd2ea20fc791
Ezequiel <zapata.ed1989@gmail.com>
2023-05-22 16:02:09 -0300

    forroot

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,9 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot( process.env.MONGODB ),
+    MongooseModule.forRoot( process.env.MONGODB, {
+      useNewUrlParser: true, useUnifiedTopology: true
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
